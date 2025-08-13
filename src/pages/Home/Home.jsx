bc5312eb86e49ebdcebddd67b64ebf23d1f30099
md-suhag <abdussalamsuhag104@gmail.com>
2025-08-13 11:29:39 +0600

    add: new arrival section in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import Category from "./categories/Category";
 import Container from "@/components/shared/Container";
 
+import NewArrival from "./NewArrival";
+
 const Home = () => {
   return (
     <Container>
       <Category />
+      <NewArrival />
     </Container>
   );
 };
